04cf60d6d74679dec413ea7627c6331d6a73cc2a
Ezekiel Kang’ombe <ezekielkangombe@gmail.com>
2026-02-21 13:15:21 +0200

    User input practice

---
+++ day3_user_input.py
@@ -0,0 +1,9 @@
+# Day 3: User Input in Python
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+country = input("Enter your country: ")
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I am from", country)
